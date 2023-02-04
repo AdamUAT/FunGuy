a9abdf675ce54dafd27ae7021359c71ea06d0983
arwtsh <arwtsh@gmail.com>
2023-02-03 23:10:56 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerPawn playerPawn;
+    [SerializeField]
+    private KeyCode up;
+    [SerializeField]
+    private KeyCode down;
+    [SerializeField]
+    private KeyCode left;
+    [SerializeField]
+    private KeyCode right;
+
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    public void ProcessInputs()
+    {
+        if (Input.GetKey(up))
+        {
+            playerPawn.MoveUp();
+        }
+
+        if (Input.GetKey(down))
+        {
+            playerPawn.MoveDown();
+        }
+
+        if (Input.GetKey(left))
+        {
+            playerPawn.MoveLeft();
+        }
+
+        if (Input.GetKey(right))
+        {
+            playerPawn.MoveRight();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPawn : MonoBehaviour
+{
+    public void MoveUp()
+    {
+
+    }    
+    public void MoveDown()
+    {
+
+    }    
+    public void MoveLeft()
+    {
+
+    }    
+    public void MoveRight()
+    {
+
+    }
+}
