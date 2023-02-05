6cb72ea25bd2d0ea977fc5bcbff5e7c72e5459d8
taiquan <etaiquan@gmail.com>
2023-02-05 00:24:02 -0700

    added whiteblood cells movement and rough bacteria tracking.

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -8,11 +8,18 @@ public class PlayerSpawn : MonoBehaviour
     private GameObject playerPrefab;
     [SerializeField]
     private UnityEngine.Tilemaps.Tilemap grid;
+    [SerializeField]
+    private List<bacteria_tracking> bacteria;
 
     // Start is called before the first frame update
     void Awake()
     {
         GameObject playerInstance = Instantiate(playerPrefab, transform.position, transform.rotation); //Spawns the player at the location of the PlayerSpawn gameobject
         playerInstance.GetComponent<PlayerPawn>().grid = this.grid; //The grid is stored in the PlayerSpawn, since the grid will be a different instance for each level.
+
+        foreach(bacteria_tracking bacter in bacteria)
+        {
+            bacter.Player = playerInstance;
+        }
     }
 }
