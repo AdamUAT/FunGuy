88cad7debef5745b3609a0c4268ddee381423ea0
Xavier Vargas <xtr3m3nerd@gmail.com>
2023-02-05 11:46:22 -0700

    Add Dialogue text to display

--- Assets/Scripts/DialogueDisplay.cs
+++ Assets/Scripts/DialogueDisplay.cs
@@ -1,112 +1,227 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogueDisplay : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject playerDialogue;
-    [SerializeField]
-    private GameObject paragraphDialogue;
-    private GameObject player;
+  [SerializeField]
+  private GameObject playerDialogue;
 
-    //The two sets of timers tells update which one's active.
-    private float playerTimer;
-    private float paragraphTimer;
-    [SerializeField]
-    private float lengthOfTimer = 5.0f; //The length of the timer in seconds
+  // [SerializeField]
+  // private GameObject paragraphDialogue;
 
-    //The function to call to display dialogue as if it was said from the character.
-    public void DisplayPlayerDialogue(string content)
+  [SerializeField]
+  private TextMeshProUGUI dialogueText;
+
+  private GameObject player;
+
+  //The two sets of timers tells update which one's active.
+  private float deactiveTime;
+
+  [SerializeField]
+  private float lengthOfTimer = 5.0f; //The length of the timer in seconds
+
+  public enum ChatEvent {
+    CasualChat,
+    RunningIntoWhiteBloodCells,
+    TrappingWhiteBloodCell,
+    MovingThroughSelf,
+    TrappedByBacteria,
+    DefeatingBacteria,
+    RunningIntoBlockade,
+    PassingBlockade
+  }
+
+  [SerializeField]
+  List<TextWeightTableEntry> casualChat;
+
+  [SerializeField]
+  List<TextWeightTableEntry> runningIntoWhiteBloodCells;
+
+  [SerializeField]
+  List<TextWeightTableEntry> trappingWhiteBloodCell;
+
+  [SerializeField]
+  List<TextWeightTableEntry> movingThroughSelf;
+
+  [SerializeField]
+  List<TextWeightTableEntry> trappedByBacteria;
+
+  [SerializeField]
+  List<TextWeightTableEntry> defeatingBacteria;
+
+  [SerializeField]
+  List<TextWeightTableEntry> runningIntoBlockade;
+
+  [SerializeField]
+  List<TextWeightTableEntry> passingBlockade;
+
+  private void Start()
+  {
+    playerDialogue.SetActive(false);
+  }
+
+  private string GetStringFromEventTable(ChatEvent eventType)
+  {
+    switch(eventType){
+      case ChatEvent.CasualChat:
+        return GetRandomValueFromWeightedTable(casualChat);
+      case ChatEvent.RunningIntoWhiteBloodCells:
+        return GetRandomValueFromWeightedTable(runningIntoWhiteBloodCells);
+      case ChatEvent.TrappingWhiteBloodCell:
+        return GetRandomValueFromWeightedTable(trappingWhiteBloodCell);
+      case ChatEvent.MovingThroughSelf:
+        return GetRandomValueFromWeightedTable(movingThroughSelf);
+      case ChatEvent.TrappedByBacteria:
+        return GetRandomValueFromWeightedTable(movingThroughSelf);
+      case ChatEvent.DefeatingBacteria:
+        return GetRandomValueFromWeightedTable(defeatingBacteria);
+      case ChatEvent.RunningIntoBlockade:
+        return GetRandomValueFromWeightedTable(runningIntoBlockade);
+      case ChatEvent.PassingBlockade:
+        return GetRandomValueFromWeightedTable(passingBlockade);
+      default:
+        throw new KeyNotFoundException("Unimpliment ChatEvent");
+    }
+  }
+
+  private string GetRandomValueFromWeightedTable(List<TextWeightTableEntry> table)
+  {
+    float totalWeight = 0.0f;
+    foreach(TextWeightTableEntry entry in table)
     {
-        ActivatePlayerDialogue();
+      totalWeight += entry.weight;
+    }
+    float roll = UnityEngine.Random.Range(0.0f, totalWeight);
 
+    float tempWeight = 0.0f;
+    foreach(TextWeightTableEntry entry in table)
+    {
+      tempWeight += entry.weight;
+      if( roll <= tempWeight) return entry.text;
     }
 
-    //The function to call to display any naration dialogue
-    public void DisplayParagraphDialogue(string content)
-    {
+    throw new ArgumentOutOfRangeException("Roll higher that the range. This should not happen");
+  }
 
+  public void TriggerChatEvent(ChatEvent eventType)
+  {
+    string content = GetStringFromEventTable(eventType);
+    DisplayPlayerDialogue(content);
+  }
+
+  //The function to call to display dialogue as if it was said from the character.
+  public void DisplayPlayerDialogue(string content)
+  {
+    ActivatePlayerDialogue();
+    dialogueText.text = content;
+  }
+
+  //Reveals the player speech bubble, scales it, moves it to the player, and then positions it so it's not out of bounds.
+  private void ActivatePlayerDialogue()
+  {
+    playerDialogue.SetActive(true);
+
+    deactiveTime = Time.time + lengthOfTimer;
+  }
+
+  //Updates the dialogue box to match the current possition of the player
+  private void UpdatePlayerDialogue()
+  {
+    if (Camera.current == null) //For some reason, Camera.current is null around 10% of the time. No clue why.
+      return; //If the camera is null, then don't do any of the below calculations.
+
+
+    Vector2 playerScreenPositionPixels = Camera.current.WorldToScreenPoint(player.transform.position);
+    Vector2 playerScreenPositionPercentage = playerScreenPositionPixels / new Vector2(Camera.current.pixelWidth, Camera.current.pixelHeight);
+    playerDialogue.GetComponent<RectTransform>().anchorMin = playerScreenPositionPercentage;
+    playerDialogue.GetComponent<RectTransform>().anchorMax = playerScreenPositionPercentage;
+
+    if (playerScreenPositionPixels.x + 500 >= Camera.current.pixelWidth && playerScreenPositionPixels.y - 400 <= 0)
+    {
+      playerDialogue.GetComponent<RectTransform>().localScale = new Vector2(-1, -1);
+      dialogueText.gameObject.GetComponent<RectTransform>().localScale = new Vector2(-1, -1);
+    }
+    else if (playerScreenPositionPixels.x + 500 >= Camera.current.pixelWidth)
+    {
+      playerDialogue.GetComponent<RectTransform>().localScale = new Vector2(-1, 1);
+      dialogueText.gameObject.GetComponent<RectTransform>().localScale = new Vector2(-1, 1);
+    }
+    else if (playerScreenPositionPixels.y - 400 <= 0)
+    {
+      playerDialogue.GetComponent<RectTransform>().localScale = new Vector2(1, -1);
+      dialogueText.gameObject.GetComponent<RectTransform>().localScale = new Vector2(1, -1);
+    }
+    else
+    {
+      playerDialogue.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
+      dialogueText.gameObject.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
+    }
+  }
+
+  private void DeactivatePlayerDialogue()
+  {
+    playerDialogue.SetActive(false);
+  }
+
+
+  //The function to call to display any naration dialogue
+  // public void DisplayParagraphDialogue(string content)
+  // {
+
+  // }
+
+  // private void ActivateParagraphDialogue()
+  // {
+
+  // }
+
+  // private void UpdateParagraphDialogue()
+  // {
+
+  // }
+
+  // private void DeactivateParagraphDialogue()
+  // {
+
+  // }
+
+  public void SetPlayer(GameObject _player)
+  {
+    player = _player;
+    PlayerCasualChatTrigger casualChatTrigger = player.GetComponent<PlayerCasualChatTrigger>();
+    if (casualChatTrigger != null)
+    {
+      casualChatTrigger.onCasualChatTrigger.AddListener( delegate { TriggerChatEvent(ChatEvent.CasualChat); } );
+    }
+  }
+
+  void Update()
+  {
+    //If the image was already deactivated, then it knows to not call Deactivate...() again.
+    if(playerDialogue.activeSelf)
+    {
+      if (Time.time > deactiveTime)
+      {
+        DeactivatePlayerDialogue();
+      } else {
+        UpdatePlayerDialogue();
+      }
     }
 
-    //Reveals the player speech bubble, scales it, moves it to the player, and then positions it so it's not out of bounds.
-    private void ActivatePlayerDialogue()
-    {
-        playerDialogue.SetActive(true);
+    // if (playerTimer > 0)
+    //   UpdateParagraphDialogue();
+    // else if (playerDialogue.activeSelf)
+    //   DeactivateParagraphDialogue();
+  }
+}
 
-        playerTimer = lengthOfTimer;
-    }
-
-    //Updates the dialogue box to match the current possition of the player
-    private void UpdatePlayerDialogue()
-    {
-        Vector2 playerScreenPositionPixels;
-        Vector2 playerScreenPositionPercentage;
-
-        if (Camera.current == null) //For some reason, Camera.current is null around 10% of the time. No clue why.
-            return; //If the camera is null, then don't do any of the below calculations.
-
-
-        playerScreenPositionPixels = Camera.current.WorldToScreenPoint(player.transform.position);
-        playerScreenPositionPercentage = playerScreenPositionPixels / new Vector2(Camera.current.pixelWidth, Camera.current.pixelHeight);
-        playerDialogue.GetComponent<RectTransform>().anchorMin = playerScreenPositionPercentage;
-        playerDialogue.GetComponent<RectTransform>().anchorMax = playerScreenPositionPercentage;
-
-        if (playerScreenPositionPixels.x + 500 >= Camera.current.pixelWidth && playerScreenPositionPixels.y - 400 <= 0)
-        {
-            playerDialogue.GetComponent<RectTransform>().localScale = new Vector2(-1, -1);
-        }
-        else if (playerScreenPositionPixels.x + 500 >= Camera.current.pixelWidth)
-        {
-            playerDialogue.GetComponent<RectTransform>().localScale = new Vector2(-1, 1);
-        }
-        else if (playerScreenPositionPixels.y - 400 <= 0)
-        {
-            playerDialogue.GetComponent<RectTransform>().localScale = new Vector2(1, -1);
-        }
-        else
-        {
-            playerDialogue.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
-        }
-
-        playerTimer -= Time.deltaTime;
-    }
-
-    private void DeactivatePlayerDialogue()
-    {
-        playerDialogue.SetActive(false);
-    }
-
-
-    private void ActivateParagraphDialogue()
-    {
-
-    }
-
-    private void UpdateParagraphDialogue()
-    {
-
-    }
-
-    private void DeactivateParagraphDialogue()
-    {
-
-    }
-
-    public void SetPlayer(GameObject _player) { player = _player; }
-
-    void Update()
-    {
-        //If the image was already deactivated, then it knows to not call Deactivate...() again.
-        if (playerTimer > 0)
-            UpdatePlayerDialogue();
-        else if (playerDialogue.activeSelf)
-            DeactivatePlayerDialogue();
-
-        if (playerTimer > 0)
-            UpdateParagraphDialogue();
-        else if (playerDialogue.activeSelf)
-            DeactivateParagraphDialogue();
-    }
+[Serializable]
+public class TextWeightTableEntry
+{
+  public float weight;
+  public string text;
 }
