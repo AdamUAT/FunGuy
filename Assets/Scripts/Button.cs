00f2ebafb6d48470e08b97de3b9fc10e9db022ec
Xavier Vargas <xtr3m3nerd@gmail.com>
2023-02-05 12:41:58 -0700

    Add Door and Dialogue sounds

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject door;
+  [SerializeField]
+  private GameObject door;
 
-    private void OnTriggerEnter2D(Collider2D other)
+  [SerializeField]
+  private SoundPlayer soundPlayer;
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    Debug.Log("lkajlkjl");
+    if (other.gameObject.CompareTag("Player"))
     {
-        Debug.Log("lkajlkjl");
-        if (other.gameObject.CompareTag("Player"))
-        {
-            door.SetActive(false);
-        }
+      if (door.activeSelf)
+      {
+        soundPlayer.gameObject.transform.position = door.transform.position;
+        soundPlayer.Play();
+        door.SetActive(false);
+      }
     }
+  }
 }
